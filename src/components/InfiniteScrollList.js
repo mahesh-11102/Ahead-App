9c4a2e4413357a7f569bf7bd21d2e19886c62954
mahesh-11102 <dasikamahesh1@gmail.com>
2023-11-16 10:42:29 +0530

    Improved Scrolling Part

--- src/components/InfiniteScrollList.js
+++ src/components/InfiniteScrollList.js
@@ -1,71 +1,58 @@
 import React, { useState, useEffect, useRef } from 'react';
-import styles from '../styles/InfiniteScrollList.module.css';
+import styles from '../styles/InfiniteScrollList.module.css'; // Ensure this path matches your CSS module
 
 const InfiniteScrollList = () => {
     const [items, setItems] = useState([]);
-    const [displayItems, setDisplayItems] = useState([]);
-    const [page, setPage] = useState(0);
     const [highlightedIndex, setHighlightedIndex] = useState(0);
     const itemListRef = useRef(null);
-    const loader = useRef(null);
 
     useEffect(() => {
-        // Fetch all data once and set items
+        // Fetch data and set items
         fetch('https://mocki.io/v1/8ed4d7b7-addf-454c-ac5c-7d873a083920')
             .then(response => response.json())
             .then(data => {
-                setItems(data);
-                setDisplayItems(data.slice(0, 5)); // Initially display first 5 items
+                setItems(data); // Assuming you want to display all items
             })
-            .catch(error => console.error(error));
+            .catch(error => console.error('Error fetching data:', error));
     }, []);
 
-    useEffect(() => {
-        const observer = new IntersectionObserver(entries => {
-            const firstEntry = entries[0];
-            if (firstEntry.isIntersecting) {
-                setPage(page => page + 1);
-            }
-        }, { threshold: 1.0 });
+    const handleScroll = () => {
+        const { scrollTop, clientHeight, scrollHeight } = itemListRef.current;
+        const totalHeight = clientHeight + scrollTop;
+        const eachItemHeight = 100; // Adjust if your item heights are different
 
-        if (loader.current) {
-            observer.observe(loader.current);
+        // Determine which item index should be highlighted
+        let currentHighlight = Math.floor(scrollTop / eachItemHeight);
+
+        // Check if the bottom of the scroll is reached and adjust highlighted index
+        if (totalHeight >= scrollHeight) {
+            currentHighlight = items.length - 1;
         }
 
-        return () => observer.disconnect();
-    }, []);
+        setHighlightedIndex(currentHighlight);
+    };
 
+    // Attach the scroll event listener when the component mounts
     useEffect(() => {
-        const nextPageItems = items.slice(page * 5, (page + 1) * 5);
-        setDisplayItems(prevItems => [...prevItems, ...nextPageItems]);
-    }, [page, items]);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            const { scrollTop } = itemListRef.current;
-            const currentIndex = Math.floor(scrollTop / 100); // Assuming each item is 100px high
-            setHighlightedIndex(currentIndex);
-        };
-
         const itemListElement = itemListRef.current;
         itemListElement.addEventListener('scroll', handleScroll);
 
+        // Remove event listener on component unmount
         return () => itemListElement.removeEventListener('scroll', handleScroll);
-    }, []);
+    }, [items.length]); // Adding items.length to ensure the effect runs when items update
 
     return (
         <div className={styles.scrollContainer}>
             <div ref={itemListRef} className={styles.itemList}>
-                {displayItems.map((item, index) => (
-                    <div key={index} className={`${styles.item} ${highlightedIndex === index ? styles.highlighted : ''}`}>
+                {items.map((item, index) => (
+                    <div key={index}
+                        className={`${styles.item} ${highlightedIndex === index ? styles.highlighted : ''}`}>
                         <h2>{item.title}</h2>
                         <p>{item.description}</p>
                     </div>
                 ))}
             </div>
-            <div ref={loader} className={styles.loader} />
-            <div className={styles.blurTop} />
-            <div className={styles.blurBottom} />
+            {/* Loader and blurs removed for simplicity. Re-add if necessary */}
         </div>
     );
 };
